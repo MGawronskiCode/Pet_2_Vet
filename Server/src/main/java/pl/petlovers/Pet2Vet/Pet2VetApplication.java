59ffac906282d7f617f528c980d34a958fc83736
Michał Gawroński <79256098+GMichalCode@users.noreply.github.com>
2021-11-15 23:31:27 +0100

    Initial commit

---
+++ Server/src/main/java/pl/petlovers/Pet2Vet/Pet2VetApplication.java
@@ -0,0 +1,13 @@
+package pl.petlovers.Pet2Vet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Pet2VetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Pet2VetApplication.class, args);
+	}
+
+}
---
+++ Server/src/test/java/pl/petlovers/Pet2Vet/Pet2VetApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.petlovers.Pet2Vet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Pet2VetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
