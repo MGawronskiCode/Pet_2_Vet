35fb073b76a90ba7275289f54a2ea328ef117d44
Michał Gawroński <79256098+GMichalCode@users.noreply.github.com>
2021-11-30 16:50:49 +0100

    Server data init

--- Server/src/main/java/pl/petlovers/Pet2Vet/pet/PetRepository.java
+++
@@ -1,8 +0,0 @@
-package pl.petlovers.Pet2Vet.pet;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface PetRepository extends JpaRepository<Pet, Long> {
-}
