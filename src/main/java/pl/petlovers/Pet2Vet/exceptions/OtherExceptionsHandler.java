20d0961a12e7fde7cd943415ee66a77de8eb23e9
Michał Gawroński <79256098+GMichalCode@users.noreply.github.com>
2021-12-15 17:16:29 +0100

    new exceptions handled

--- src/main/java/pl/petlovers/Pet2Vet/exceptions/OtherExceptionsHandler.java
+++ src/main/java/pl/petlovers/Pet2Vet/exceptions/OtherExceptionsHandler.java
@@ -1,7 +1,10 @@
 package pl.petlovers.Pet2Vet.exceptions;
 
 import lombok.extern.slf4j.Slf4j;
+import org.hibernate.exception.ConstraintViolationException;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -37,6 +40,47 @@ public class OtherExceptionsHandler {
   String illegalStateHandler(IllegalStateException exception) {
     log.error(ERROR + exception.toString());
     return exception.getMessage();
+  }
+
+  @ResponseBody
+  @ExceptionHandler(HttpMessageNotReadableException.class)
+  @ResponseStatus(HttpStatus.NOT_ACCEPTABLE)
+  String httpMessageNotReadableException(HttpMessageNotReadableException exception) {
+    log.error(ERROR + exception.toString());
+    return exception.getMessage();
+  }
+
+  @ResponseBody
+  @ExceptionHandler(NumberFormatException.class)
+  @ResponseStatus(HttpStatus.BAD_REQUEST)
+  String numberFormatException(NumberFormatException exception) {
+    log.error(ERROR + exception.toString());
+    return exception.getMessage();
+  }
+
+  @ResponseBody
+  @ExceptionHandler(DataIntegrityViolationException.class)
+  @ResponseStatus(HttpStatus.BAD_REQUEST)
+  String dtaIntegrityViolationException(DataIntegrityViolationException exception) {
+    log.error(ERROR + exception.toString());
+    return "Could not execute statement, remember to send the data required in this query. If you are trying to delete something, make sure it is not included in some objects";
+  }
+
+  @ResponseBody
+  @ExceptionHandler(ConstraintViolationException.class)
+  @ResponseStatus(HttpStatus.BAD_REQUEST)
+  String constraintViolationException(ConstraintViolationException exception) {
+    log.error(ERROR + exception.toString());
+    return exception.getMessage();
+  }
+
+  @ResponseBody
+  @ExceptionHandler(Exception.class)
+  @ResponseStatus(HttpStatus.FORBIDDEN)
+  String otherException() {
+    final String message = "unknown error";
+    log.error(ERROR + message);
+    return message;
   }
 
 }
