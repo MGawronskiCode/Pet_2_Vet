662a43061e30c3c20e020c41b5a94c07949cb26b
Michał Gawroński <79256098+GMichalCode@users.noreply.github.com>
2021-12-15 21:49:46 +0100

    get with user and pet id added

--- src/main/java/pl/petlovers/Pet2Vet/exceptions/unautorized_exceptions/PetUnauthorizedAttemptException.java
+++ src/main/java/pl/petlovers/Pet2Vet/exceptions/unautorized_exceptions/PetUnauthorizedAttemptException.java
@@ -1,7 +1,11 @@
 package pl.petlovers.Pet2Vet.exceptions.unautorized_exceptions;
 
 public class PetUnauthorizedAttemptException extends UnauthorizedAttemptException {
-  protected PetUnauthorizedAttemptException(String message) {
+  public PetUnauthorizedAttemptException(String message) {
     super(message);
+  }
+
+  public PetUnauthorizedAttemptException(long userId, long petId) {
+    super("User with id = " + userId + " has no pet with id = " + petId);
   }
 }
