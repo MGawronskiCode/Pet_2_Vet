6a7e8819fcccb1d526a2a641eb118e38847f5ceb
Michał Gawroński <79256098+GMichalCode@users.noreply.github.com>
2021-12-30 11:52:11 +0100

    simple permissions for test added csrf disabled (made it possible to send requests with authorization from another service)

--- src/main/java/pl/petlovers/Pet2Vet/security/SecurityConfig.java
+++ src/main/java/pl/petlovers/Pet2Vet/security/SecurityConfig.java
@@ -1,7 +1,8 @@
-package pl.petlovers.Pet2Vet;
+package pl.petlovers.Pet2Vet.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.User;
@@ -12,18 +13,22 @@ import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 @Configuration
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
+  private static final String ADMIN = "ADMIN";
+  private static final String OWNER = "OWNER";
+
   @Bean
+  @Override
   public UserDetailsService userDetailsService() {
 //    fixme change deprecated method storing plaintext data in memory into protected data stored in db (use bcrypt)
     UserDetails adminDetails = User.withDefaultPasswordEncoder()
         .username("admin")
         .password("123")
-        .roles("ADMIN")
+        .roles(ADMIN)
         .build();
     UserDetails ownerDetails = User.withDefaultPasswordEncoder()
         .username("owner")
         .password("123")
-        .roles("OWNER")
+        .roles(OWNER)
         .build();
 
     return new InMemoryUserDetailsManager(adminDetails, ownerDetails);
@@ -31,13 +36,17 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
   @Override
   protected void configure(HttpSecurity http) throws Exception {
-    http.authorizeRequests()
-        .antMatchers("/users").permitAll()
-        .antMatchers("/pets").hasAnyRole("ADMIN", "OWNER")
-        .anyRequest().hasAnyRole("ADMIN")
+    http.httpBasic().and().authorizeRequests()
+        .antMatchers(HttpMethod.GET).permitAll()
+        .antMatchers(HttpMethod.POST).hasAnyRole(ADMIN, OWNER)
+        .antMatchers(HttpMethod.PUT).hasAnyRole(ADMIN)
+        .antMatchers(HttpMethod.DELETE).hasRole(ADMIN)
+        .anyRequest().permitAll()
         .and()
         .formLogin().permitAll()
         .and()
-        .logout().permitAll();
+        .logout().permitAll()
+        .and()
+        .csrf().disable();
   }
 }
