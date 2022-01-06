e2eba652ef5ecf3d615c11e4e63730fa41adbcd7
Michał Gawroński <79256098+GMichalCode@users.noreply.github.com>
2022-01-06 22:33:08 +0100

    PetServiceTest trying to resolve problems with null pointer exceptions

---
+++ src/main/java/pl/petlovers/Pet2Vet/security/users/AppUserDetails.java
@@ -0,0 +1,9 @@
+package pl.petlovers.Pet2Vet.security.users;
+
+import pl.petlovers.Pet2Vet.appUser.AppUser;
+
+public class AppUserDetails {
+  public AppUser getAppUser(){
+    return new AppUser();
+  }
+}
