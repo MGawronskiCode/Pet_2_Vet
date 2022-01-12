a59e9b6b30f3f08b045bf60edbf44c49f95c3ad0
Michał Gawroński <79256098+GMichalCode@users.noreply.github.com>
2022-01-12 22:26:17 +0100

    NoteController secured

--- src/main/java/pl/petlovers/Pet2Vet/note/controller/NoteController.java
+++ src/main/java/pl/petlovers/Pet2Vet/note/controller/NoteController.java
@@ -2,9 +2,13 @@ package pl.petlovers.Pet2Vet.note.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.security.access.annotation.Secured;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
+import pl.petlovers.Pet2Vet.exceptions.forbidden_exceptions.NoteForbiddenAccessException;
 import pl.petlovers.Pet2Vet.note.NoteService;
+import pl.petlovers.Pet2Vet.pet.Pet;
+import pl.petlovers.Pet2Vet.pet.PetService;
 import pl.petlovers.Pet2Vet.security.users.AppUserDetails;
 
 import java.util.List;
@@ -14,117 +18,225 @@ import java.util.List;
 public class NoteController {
 
     private final NoteService noteService;
+    private final PetService petService;
 
     @Autowired
-    public NoteController(NoteService noteService) {
+    public NoteController(NoteService noteService, PetService petService) {
         this.noteService = noteService;
+        this.petService = petService;
     }
 
+    @Secured({"ROLE_ADMIN", "ROLE_OWNER", "ROLE_VET", "ROLE_KEEPER"})
     @ResponseStatus(HttpStatus.OK)
-    @GetMapping("/notes")
-    public List<NoteDTO> getAllUserNotes(@AuthenticationPrincipal AppUserDetails loggedUser) {
+    @GetMapping("/users/{userId}/notes")
+    public List<NoteDTO> getAllUserNotes(@PathVariable long userId, @AuthenticationPrincipal AppUserDetails loggedUser) {
 
-        long userId = loggedUser.getId();
+        if (loggedUserIsAdminOrNoteBelongsToHim(userId, loggedUser)) {
 
-        return noteService.getAllUserNotes(userId)
-            .stream()
-            .map(NoteDTO::of)
-            .toList();
+            return noteService.getAllUserNotes(userId)
+                .stream()
+                .map(NoteDTO::of)
+                .toList();
+        } else {
+
+            throw new NoteForbiddenAccessException();
+        }
+
     }
 
+    @Secured({"ROLE_ADMIN", "ROLE_OWNER", "ROLE_VET", "ROLE_KEEPER"})
+    @ResponseStatus(HttpStatus.OK)
+    @GetMapping("users/{userId}/pets/notes")
+    public List<NoteDTO> getAllUserPetsNotes(@PathVariable long userId, @AuthenticationPrincipal AppUserDetails loggedUser) {
+
+        if (loggedUserIsAdminOrNoteBelongsToHim(userId, loggedUser)) {
+
+            return noteService.getAllUserPetsNotes(userId)
+                .stream()
+                .map(NoteDTO::of)
+                .toList();
+        } else {
+
+            throw new NoteForbiddenAccessException();
+        }
+    }
+
+    @Secured({"ROLE_ADMIN", "ROLE_OWNER", "ROLE_VET", "ROLE_KEEPER"})
     @ResponseStatus(HttpStatus.OK)
     @GetMapping("/pets/{petId}/notes")
-    public List<NoteDTO> getAllPetNotes(@PathVariable long petId) {
-        return noteService.getAllPetNotes(petId)
+    public List<NoteDTO> getAllPetNotes(@PathVariable long petId, @AuthenticationPrincipal AppUserDetails loggedUser) {
+
+        if (loggedUserIsAdminOrNoteBelongsToHisPet(petId, loggedUser)) {
+
+            return noteService.getAllPetNotes(petId)
                 .stream()
                 .map(NoteDTO::of)
                 .toList();
+        } else {
+            throw new NoteForbiddenAccessException();
+        }
     }
 
+    @Secured({"ROLE_ADMIN", "ROLE_OWNER", "ROLE_VET", "ROLE_KEEPER"})
     @ResponseStatus(HttpStatus.OK)
-    @GetMapping("/pets/notes")
-    public List<NoteDTO> getAllUserPetsNotes(@AuthenticationPrincipal AppUserDetails loggedUser) {
+    @GetMapping("/users/{userId}/notes/{noteId}")
+    public NoteDTO getUserNote(@PathVariable long userId, @PathVariable long noteId, @AuthenticationPrincipal AppUserDetails loggedUser) {
 
-        long userId = loggedUser.getId();
+        if (loggedUserIsAdminOrNoteBelongsToHim(userId, loggedUser)) {
 
-        return noteService.getAllUserPetsNotes(userId)
-                .stream()
-                .map(NoteDTO::of)
-                .toList();
+            return NoteDTO.of(noteService.getUserNote(userId, noteId));
+        } else {
+
+            throw new NoteForbiddenAccessException();
+        }
     }
 
+    @Secured({"ROLE_ADMIN", "ROLE_OWNER", "ROLE_VET", "ROLE_KEEPER"})
     @ResponseStatus(HttpStatus.OK)
     @GetMapping("/pets/{petId}/notes/{noteId}")
-    public NoteDTO getPetNote(@PathVariable long petId, @PathVariable long noteId) {
-        return NoteDTO.of(noteService.getPetNote(petId, noteId));
+    public NoteDTO getPetNote(@PathVariable long petId, @PathVariable long noteId, @AuthenticationPrincipal AppUserDetails loggedUser) {
+
+        if (loggedUserIsAdminOrNoteBelongsToHisPet(petId, loggedUser)) {
+
+            return NoteDTO.of(noteService.getPetNote(petId, noteId));
+        } else {
+            throw new NoteForbiddenAccessException();
+        }
     }
 
-    @ResponseStatus(HttpStatus.OK)
-    @GetMapping("/notes/{noteId}")
-    public NoteDTO getUserNote(@AuthenticationPrincipal AppUserDetails loggedUser, @PathVariable long noteId) {
+    @Secured({"ROLE_ADMIN", "ROLE_OWNER", "ROLE_VET", "ROLE_KEEPER"})
+    @ResponseStatus(HttpStatus.CREATED)
+    @PostMapping("/users/{userId}/notes")
+    public NoteDTO createUserNote(@PathVariable long userId, @RequestBody NoteDTO noteDTO, @AuthenticationPrincipal AppUserDetails loggedUser) {
 
-        long userId = loggedUser.getId();
+        if (loggedUserIsAdminOrNoteBelongsToHim(userId, loggedUser)) {
 
-        return NoteDTO.of(noteService.getUserNote(userId, noteId));
+            return NoteDTO.of(noteService.createUserNote(userId, noteDTO.toNote()));
+
+        } else {
+
+            throw new NoteForbiddenAccessException();
+        }
     }
 
+    @Secured({"ROLE_ADMIN", "ROLE_OWNER", "ROLE_VET", "ROLE_KEEPER"})
     @ResponseStatus(HttpStatus.CREATED)
     @PostMapping("/pets/{petId}/notes")
-    public NoteDTO createPetNote(@PathVariable long petId, @RequestBody NoteDTO noteDTO) {
-        return NoteDTO.of(noteService.createPetNote(petId, noteDTO.toNote()));
+    public NoteDTO createPetNote(@PathVariable long petId, @RequestBody NoteDTO noteDTO, @AuthenticationPrincipal AppUserDetails loggedUser) {
+
+        if (loggedUserIsAdminOrNoteBelongsToHisPet(petId, loggedUser)) {
+
+            return NoteDTO.of(noteService.createPetNote(petId, noteDTO.toNote()));
+        } else {
+            throw new NoteForbiddenAccessException();
+        }
     }
 
+    @Secured({"ROLE_ADMIN", "ROLE_OWNER", "ROLE_VET", "ROLE_KEEPER"})
     @ResponseStatus(HttpStatus.CREATED)
-    @PostMapping("/notes")
-    public NoteDTO createUserNote(@AuthenticationPrincipal AppUserDetails loggedUser, @RequestBody NoteDTO noteDTO) {
+    @PutMapping("/users/{userId}/notes/{noteId}")
+    public NoteDTO updateUserNote(@PathVariable long userId, @PathVariable long noteId, @RequestBody NoteDTO noteDTO, @AuthenticationPrincipal AppUserDetails loggedUser) {
 
-        long userId = loggedUser.getId();
+        if (loggedUserIsAdminOrNoteBelongsToHim(userId, loggedUser)) {
 
-        return NoteDTO.of(noteService.createUserNote(userId, noteDTO.toNote()));
+            return NoteDTO.of(noteService.updateUserNote(userId, noteId, noteDTO.toNote()));
+
+        } else {
+
+            throw new NoteForbiddenAccessException();
+        }
     }
 
+    @Secured({"ROLE_ADMIN", "ROLE_OWNER", "ROLE_VET", "ROLE_KEEPER"})
     @ResponseStatus(HttpStatus.CREATED)
     @PutMapping("/pets/{petId}/notes/{noteId}")
-    public NoteDTO updatePetNote(@PathVariable long petId, @PathVariable long noteId, @RequestBody NoteDTO noteDTO) {
-        return NoteDTO.of(noteService.updatePetNote(petId, noteId, noteDTO.toNote()));
+    public NoteDTO updatePetNote(@PathVariable long petId, @PathVariable long noteId, @RequestBody NoteDTO noteDTO, @AuthenticationPrincipal AppUserDetails loggedUser) {
+
+        if (loggedUserIsAdminOrNoteBelongsToHisPet(petId, loggedUser)) {
+
+            return NoteDTO.of(noteService.updatePetNote(petId, noteId, noteDTO.toNote()));
+        } else {
+            throw new NoteForbiddenAccessException();
+        }
     }
 
-    @ResponseStatus(HttpStatus.CREATED)
-    @PutMapping("/notes/{noteId}")
-    public NoteDTO updateUserNote(@AuthenticationPrincipal AppUserDetails loggedUser, @PathVariable long noteId, @RequestBody NoteDTO noteDTO) {
-
-        long userId = loggedUser.getId();
-
-        return NoteDTO.of(noteService.updateUserNote(userId, noteId, noteDTO.toNote()));
-    }
-
+    @Secured({"ROLE_ADMIN", "ROLE_OWNER", "ROLE_VET", "ROLE_KEEPER"})
     @ResponseStatus(HttpStatus.NO_CONTENT)
-    @DeleteMapping("/notes")
-    public void deleteUserNotes(@AuthenticationPrincipal AppUserDetails loggedUser) {
+    @DeleteMapping("/users/{userId}/notes")
+    public void deleteUserNotes(@PathVariable long userId, @AuthenticationPrincipal AppUserDetails loggedUser) {
 
-        long userId = loggedUser.getId();
+        if (loggedUserIsAdminOrNoteBelongsToHim(userId, loggedUser)) {
 
-        noteService.deleteUserNotes(userId);
+            noteService.deleteUserNotes(userId);
+        } else {
+
+            throw new NoteForbiddenAccessException();
+        }
     }
 
+    @Secured({"ROLE_ADMIN", "ROLE_OWNER", "ROLE_VET", "ROLE_KEEPER"})
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    @DeleteMapping("/users/{userId}/notes/{noteId}")
+    public void deleteUserNote(@PathVariable long userId, @PathVariable long noteId, @AuthenticationPrincipal AppUserDetails loggedUser) {
+
+        if (loggedUserIsAdminOrNoteBelongsToHim(userId, loggedUser)) {
+
+            noteService.deleteUserNote(userId, noteId);
+        } else {
+
+            throw new NoteForbiddenAccessException();
+        }
+    }
+
+    @Secured({"ROLE_ADMIN", "ROLE_OWNER", "ROLE_VET", "ROLE_KEEPER"})
     @ResponseStatus(HttpStatus.NO_CONTENT)
     @DeleteMapping("/pets/{petId}/notes")
-    public void deletePetNotes(@PathVariable long petId) {
-        noteService.deletePetNotes(petId);
+    public void deletePetNotes(@PathVariable long petId, @AuthenticationPrincipal AppUserDetails loggedUser) {
+
+        if (loggedUserIsAdminOrNoteBelongsToHisPet(petId, loggedUser)) {
+
+            noteService.deletePetNotes(petId);
+        } else {
+            throw new NoteForbiddenAccessException();
+        }
     }
 
+    @Secured({"ROLE_ADMIN", "ROLE_OWNER", "ROLE_VET", "ROLE_KEEPER"})
     @ResponseStatus(HttpStatus.NO_CONTENT)
     @DeleteMapping("/pets/{petId}/notes/{noteId}")
-    public void deletePetNote(@PathVariable long petId, @PathVariable long noteId) {
-        noteService.deletePetNote(petId, noteId);
+    public void deletePetNote(@PathVariable long petId, @PathVariable long noteId, @AuthenticationPrincipal AppUserDetails loggedUser) {
+
+        if (loggedUserIsAdminOrNoteBelongsToHisPet(petId, loggedUser)) {
+
+            noteService.deletePetNote(petId, noteId);
+        } else {
+            throw new NoteForbiddenAccessException();
+        }
     }
 
-    @ResponseStatus(HttpStatus.NO_CONTENT)
-    @DeleteMapping("/notes/{noteId}")
-    public void deleteUserNote(@AuthenticationPrincipal AppUserDetails loggedUser, @PathVariable long noteId) {
+    private boolean loggedUserIsAdminOrNoteBelongsToHim(long userId, AppUserDetails loggedUser) {
 
-        long userId = loggedUser.getId();
+        return loggedUser.isAdmin() || noteBelongsToLoggedUser(userId, loggedUser);
+    }
 
-        noteService.deleteUserNote(userId, noteId);
+    private boolean noteBelongsToLoggedUser(long userId, AppUserDetails loggedUser) {
+
+        return loggedUser.getId() == userId;
+    }
+
+    private boolean loggedUserIsAdminOrNoteBelongsToHisPet(long petId, AppUserDetails loggedUser) {
+
+        return loggedUser.isAdmin() || noteBelongsToPetOfLoggedUser(petId, loggedUser);
+    }
+
+    private boolean noteBelongsToPetOfLoggedUser (long petId, AppUserDetails loggedUser) {
+
+        Pet pet = petService.get(petId);
+
+        return loggedUserContainsPet(loggedUser, pet);
+    }
+
+    private boolean loggedUserContainsPet(AppUserDetails loggedUser, Pet pet) {
+        return loggedUser.getAppUser().getPets().contains(pet);
     }
 }
