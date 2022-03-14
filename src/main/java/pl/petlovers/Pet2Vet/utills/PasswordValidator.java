08b3e292768c8df9cb776a8c249f3377018d2f3b
Michał Gawroński <79256098+GMichalCode@users.noreply.github.com>
2022-03-14 11:55:44 +0100

    PasswordValidator code refactoring

--- src/main/java/pl/petlovers/Pet2Vet/utills/PasswordValidator.java
+++ src/main/java/pl/petlovers/Pet2Vet/utills/PasswordValidator.java
@@ -2,16 +2,39 @@ package pl.petlovers.Pet2Vet.utills;
 
 import pl.petlovers.Pet2Vet.utills.exceptions.PasswordNonValidPatternException;
 
+import static java.lang.Character.isDigit;
+
 public class PasswordValidator {
 
+  private PasswordValidator() {}
 
   public static boolean validPassword(String password) {
-    if (password == null) {
-      throw new PasswordNonValidPatternException();
+
+    if ((notNull(password) && sufficientlyLongPassword(password) && containsDigit(password))) {
+      return true;
     }
 
-    if (password.length() < 5) {
+    throw new PasswordNonValidPatternException();
+  }
 
+  private static boolean notNull(String password) {
+
+    return password != null;
+  }
+
+  private static boolean sufficientlyLongPassword(String password) {
+    final int MINIMUM_PASSWORD_LENGTH = 5;
+
+    return password.length() >= MINIMUM_PASSWORD_LENGTH;
+  }
+
+  private static boolean containsDigit(String password) {
+    for (int i = 0; i < password.length(); i++) {
+      if (isDigit(password.charAt(i))) {
+        return true;
+      }
     }
+
+    return false;
   }
 }
